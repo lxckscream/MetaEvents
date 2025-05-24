659a6462ef0abf9ff6a1479317d8c58a8184219b
don1x <mityafotografer@gmail.com>
2025-05-24 23:39:16 +0300

    1

--- src/main/java/ru/don1x/metaevents/exceptions/NotAssignableFromAddonClass.java
+++ src/main/java/ru/don1x/metaevents/exceptions/NotAssignableFromAddonClass.java
@@ -2,7 +2,7 @@ package ru.don1x.metaevents.exceptions;
 
 import ru.don1x.metaevents.addon.Addon;
 
-public class NotAssignableFromAddonClass extends RuntimeException {
+public final class NotAssignableFromAddonClass extends RuntimeException {
     public NotAssignableFromAddonClass(Class<?> clazz) {
         super("The class " + clazz.getPackage() + " not assignable from " + Addon.class.getPackage());
     }
