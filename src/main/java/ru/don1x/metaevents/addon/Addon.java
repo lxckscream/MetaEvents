41f37d3c23ed2a832a8d13e7b59735c0ea7bbd2e
don1x <mityafotografer@gmail.com>
2025-05-24 23:20:46 +0300

    1

---
+++ src/main/java/ru/don1x/metaevents/addon/Addon.java
@@ -0,0 +1,15 @@
+package ru.don1x.metaevents.addon;
+
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@Getter
+public abstract class Addon {
+    private final JavaPlugin plugin;
+    private final String name;
+
+    public Addon(JavaPlugin plugin, String name) {
+        this.name= name;
+        this.plugin = plugin;
+    }
+}
