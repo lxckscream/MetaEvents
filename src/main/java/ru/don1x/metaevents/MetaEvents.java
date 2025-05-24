472c17eb81b8ed276e68eb736370d4da0b20439e
don1x <mityafotografer@gmail.com>
2025-05-24 23:18:58 +0300

    1

---
+++ src/main/java/ru/don1x/metaevents/MetaEvents.java
@@ -0,0 +1,17 @@
+package ru.don1x.metaevents;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MetaEvents extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
